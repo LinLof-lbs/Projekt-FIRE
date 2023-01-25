562aefb89d6556ab6ee32710c1dc542334f12e84
Vilhelmternevik <Vilhelm.ternevik@elev.ga.lbs.se>
2023-01-25 14:06:28 +0100

    Update vilhelm_face_swap.cs

---
+++ Project-fire/Assets/Vilhelm/vilhelm_face_swap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vilhelm_face_swap : MonoBehaviour
+{
+    public Sprite angry;
+    public Sprite happy;
+
+    public void Angry()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = angry;
+    }
+
+    public void Happy()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = happy;
+    }
+}
